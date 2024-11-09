4bb38ca50a05ac530049df52397094bb80346835
anas-md <anasdaud1996@gmail.com>
2024-11-09 13:43:38 +0530

    chore: File refractor

--- app/workflow/_components/nodes/NodeIO.tsx
+++ app/workflow/_components/nodes/NodeIO.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function NodeInputs({ children }: { children: React.ReactNode }) {
+function NodeIO({ children }: { children: React.ReactNode }) {
   return <div className="flex flex-col divide-y gap-2">{children}</div>;
 }
 
-export default NodeInputs;
+export default NodeIO;
