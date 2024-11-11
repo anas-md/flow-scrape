e97f56b8ec9d6d1c8c0b6a3cf1b7a8ebc740b59b
anas-md <anasdaud1996@gmail.com>
2024-11-11 22:05:16 +0530

    feat: getWorkflowPhaseDetails

--- actions/workflows.ts
+++ actions/workflows.ts
@@ -132,3 +132,20 @@ export async function getWorkflowExecutionWithPhases(executionId: string) {
     },
   });
 }
+
+export async function getWorkflowPhaseDetails(phaseId: string) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthenticated");
+  }
+
+  return prisma.executionPhase.findUnique({
+    where: {
+      id: phaseId,
+      execution: {
+        userId,
+      },
+    },
+  });
+}
