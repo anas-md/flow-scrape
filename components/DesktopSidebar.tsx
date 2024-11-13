427fefd754947b8116b31b3001c4e51c3d1699b1
anas-md <anasdaud1996@gmail.com>
2024-11-13 21:44:55 +0530

    chore: typo

--- components/DesktopSidebar.tsx
+++ components/DesktopSidebar.tsx
@@ -28,7 +28,7 @@ function DesktopSidebar() {
         {routes.map((route) => (
           <Link
             key={route.href}
-            href={""}
+            href={route.href}
             className={buttonVariants({
               variant:
                 activeRoute.href === route.href
