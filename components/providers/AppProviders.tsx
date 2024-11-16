c6ee2374f16cd763d7ad74e5195635984ff2597e
anas-md <anasdaud1996@gmail.com>
2024-11-16 22:07:07 +0530

    fix: reactQuery devTools removed

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
-import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider } from "next-themes";
 import NextTopLoader from "nextjs-toploader";
+import React, { useState } from "react";
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   const [queryClient, setQueryClient] = useState(() => new QueryClient());
@@ -15,7 +14,6 @@ export function AppProviders({ children }: { children: React.ReactNode }) {
       <ThemeProvider attribute="class" defaultTheme="system">
         {children}
       </ThemeProvider>
-      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
